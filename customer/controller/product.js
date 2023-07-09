58d9ba33c2c528584dab4fc0f33a5052da9b39e0
mammamon <quangnguyenonlinevn@gmail.com>
2023-07-10 00:44:16 +0700

    update

--- customer/controller/product.js
+++ customer/controller/product.js
@@ -1,54 +1,21 @@
-function getProductDetails(productName) {
-    axios
-      .get(`https://649d36a19bac4a8e669d62a2.mockapi.io/product/${productName}`)
-      .then(function (response) {
-        const product = response.data;
-  
-        // Update the product name in the breadcrumb
-        $('#product-name').text(product.name);
-  
-        // Update the product image
-        $('.swiper-slide img').attr('src', product.image);
-  
-        // Update the product details
-        $('#branch').text(product.branch);
-        $('#title').text(product.title);
-        $('#price').text(product.price.toLocaleString());
-        $('#speed').text(product.speed);
-        $('#type').text(product.type);
-        $('#color').text(product.color);
-        $('#paper').text(product.paper);
-        $('#ram').text(product.spec.RAM);
-        $('#hdd').text(product.spec.HDD);
-        $('#dpi').text(product.spec.DPI);
-        $('#tray').text(product.spec.tray);
-        $('#warmUpTime').text(product.spec.warmUpTime);
-        $('#dspf').text(product.option.DSPF);
-        $('#rspf').text(product.option.RSPF);
-        $('#finisher').text(product.option.finisher);
-        $('#fax').text(product.option.fax);
-        $('#solution').text(product.option.solution);
-        $('#addHdd').text(product.option.addHDD);
-        $('#addRam').text(product.option.addRam);
-        $('#addStand').text(product.option.addStand);
-  
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
+// your-script.js
+// Initialize Page.js
+page();
 
+// Define the route for the product page
+page('/', productPage);
 
-// handle routing
-  function handleRouting() {
-    const pathname = window.location.pathname;
-    const productSlug = pathname.substring(pathname.lastIndexOf('/') + 1);
-    getProductDetails(productSlug);
-  }
-  
-  // Listen for the 'popstate' event when the URL changes
-  window.onpopstate = handleRouting;
-  
-  // Trigger the initial routing when the page loads
-  handleRouting();
-  
+// Define the route for the child links
+page('/child/:id', productPage);
+
+// Define the handler function for the product page and child links
+function productPage(ctx) {
+  // Load the product.html file and display it in your application
+  // This can be done using an AJAX request or by simply redirecting to the product.html page
+  // Make sure to replace ":id" with the actual product ID in the URL
+  var productUrl = 'product.html';  // or use an absolute path if necessary
+  window.location.href = productUrl;
+}
+
+// Start Page.js
+page.start();

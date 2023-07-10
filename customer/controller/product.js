bd1485e96507a52819ded45348ed5413a803d1a3
mammamon <quangnguyenonlinevn@gmail.com>
2023-07-10 12:39:09 +0700

    big update

--- customer/controller/product.js
+++ customer/controller/product.js
@@ -1,25 +1,51 @@
-// product.js
-// Initialize Page.js
-page();
-
-// Define the route for the product page
-page('/', productPage);
-
-// Define the route for the child links
-page('/child/:id', productPage);
-
-// Define the handler function for the product page and child links
-function productPage(ctx) {
-  // Fetch the contents of product.html using Axios
-  axios.get('product.html')
-    .then(function (response) {
-      // Update the current page's HTML with the fetched content
-      $('html').html(response.data);
-    })
-    .catch(function (error) {
-      console.error('Error fetching product.html:', error);
+// Function to fetch and display product data
+function fetchProductData(productName) {
+    // Make an Axios GET request to fetch the product data from the API
+    axios
+      .get('https://649d36a19bac4a8e669d62a2.mockapi.io/product/' + productName)
+      .then(function (response) {
+        var productData = response.data;
+  
+        // Update the elements in #product-show with the fetched data
+        $('#product-name').text(productData.title);
+        $('#branch').text(productData.branch);
+        $('#title').text(productData.title);
+        $('#price').text(productData.price);
+        $('#speed').text(productData.speed);
+        $('#type').text(productData.type);
+        $('#color').text(productData.color);
+        $('#paper').text(productData.paper);
+        $('#ram').text(productData.spec.RAM);
+        $('#hdd').text(productData.spec.HDD);
+        $('#dpi').text(productData.spec.DPI);
+        $('#tray').text(productData.spec.tray);
+        $('#warmUpTime').text(productData.spec.warmUpTime);
+        $('#dspf').text(productData.option.DSPF);
+        $('#rspf').text(productData.option.RSPF);
+        $('#finisher').text(productData.option.finisher);
+        $('#fax').text(productData.option.fax);
+        $('#solution').text(productData.option.solution);
+        $('#addHdd').text(productData.option.addHDD);
+        $('#addRam').text(productData.option.addRam);
+        $('#addStand').text(productData.option.addStand);
+      })
+      .catch(function (error) {
+        console.error('Error fetching product data:', error);
+      });
+  }
+  
+  // Function to initialize Page.js and define routes
+  function initializePage() {
+    page();
+  
+    // Define the route for product subpages
+    page('/product/:productName', function (ctx) {
+      var productName = ctx.params.productName;
+      fetchProductData(productName);
     });
-}
-
-// Start Page.js
-page.start();
+  }
+  
+  // Call the initializePage function when the DOM is ready
+  $(document).ready(function () {
+    initializePage();
+  });  
